6e69f6be25c5cd63e340ab6997e68563dadf5e12
unknown <maxhaviland@gmail.com>
2020-05-17 09:20:16 -0300

    removing => comments

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,20 +1,20 @@
 import styled from 'styled-components';
 
 export const Card = styled.section`
-  box-shadow: ${({outlined}) => outlined ? '1px 1px 5px 1px rgba(212,212,212,1)' : ''};
-  margin: ${({center}) => center && '0 auto' || ''};
-  display: ${({display}) => display || ''};
-  width: ${({width}) => width || 'auto'};
+  box-shadow: ${({ outlined }) => outlined ? '1px 1px 5px 1px rgba(212,212,212,1)' : ''};
+  margin: ${({ center }) => center && '0 auto' || ''};
+  display: ${({ display }) => display || ''};
+  width: ${({ width }) => width || 'auto'};
   padding-top: 2rem;
   word-break: break-all;
-  justify-content: ${({center}) => center && 'center' || ''};
+  justify-content: ${({ center }) => center && 'center' || ''};
 `;
 
 
 export const CardItem = styled.section`
-  display: ${({display}) => display || ''};
-  width: ${({width}) => width || 'auto'};
-  margin: ${({margin}) => margin || ''};
+  display: ${({ display }) => display || ''};
+  width: ${({ width }) => width || 'auto'};
+  margin: ${({ margin }) => margin || ''};
   
   @media screen and (max-width: 900px) {
     display: block;
@@ -24,21 +24,3 @@ export const CardItem = styled.section`
     justify-content: center;
   }
 `;
-
-
-/*
-export const CardList = styled.ol`
-  display: ${({flex}) => flex && 'flex' || ''};
-  padding: 0;
-`;
-
-export const CardItem = styled.li`
-  list-style: none;
-  width: ${({width}) => width + 'px' || 'auto'};
-
-`;
-
-export const CardContent = styled.section`
-  margin: 2rem;
-`
-*/
